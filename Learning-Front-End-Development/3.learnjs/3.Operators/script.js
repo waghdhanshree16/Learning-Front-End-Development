a173a720223462ec660b76006303f5df5c36b767
dhanshree <dhanshreewagh16598@gmail.com>
2023-09-29 15:10:42 +0530

    intial commit

---
+++ Learning-Front-End-Development/3.learnjs/3.Operators/script.js
@@ -0,0 +1,19 @@
+const firstNumber = 3;
+const secondNumber = 4;
+
+// const result = firstnumber + secondNumber;
+// const result = firstnumber % secondNumber;
+
+const result = firstNumber == secondNumber;
+
+//console.log(result );
+
+// console.log(result);
+ 
+// 3 = 0*4 + 3
+
+let thirdNumber = 12;   
+thirdNumber **= 4; //thirdNumber + 4;
+
+console.log(thirdNumber)
+
