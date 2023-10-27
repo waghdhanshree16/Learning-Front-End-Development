ac2d232da823ace15abadf0c6c3819077474d684
dhanshree <dhanshreewagh16598@gmail.com>
2023-10-27 16:09:53 +0530

    switch statement

---
+++ Learning-Front-End-Development/3.learnjs/5.Switch statement/script.js
@@ -0,0 +1,19 @@
+const grade = "D";
+
+switch(grade) {
+    case "A":
+    console.log("A -> very good")
+break;
+    case "B":
+    console.log("A -> Good, keep learing")
+break;
+    case "C":
+    console.log("C -> Need Improvement")
+break;
+    case "D":
+        case "E":
+    console.log("D -> Not Good")
+    break;
+    default:
+        console.log("Invaild Grade")
+}
