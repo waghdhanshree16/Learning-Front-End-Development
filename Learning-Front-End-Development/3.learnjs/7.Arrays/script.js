eb9e3143c1b3035086fdc65d1593e66ff569be8c
dhanshree <dhanshreewagh16598@gmail.com>
2023-12-06 15:05:06 +0530

    Arrays

---
+++ Learning-Front-End-Development/3.learnjs/7.Arrays/script.js
@@ -0,0 +1,30 @@
+const nums = [4, 1, 2, 8, 2, 7, 6 ];
+
+//console.log(nums.indexOf(7));
+//console.log(nums.includes(70));
+
+//nums.push('Dhara');
+//nums.pop();
+//nums.shift();
+//nums.sort();
+
+const subArray = nums.slice(2, 4);
+console.log(subArray)
+
+/*const words = ["Apple", "Banana", "Cherry",12, true,
+
+{ 
+    name: "Dhara",
+},
+ function hello() {
+    console.log("hello world");
+ },
+
+];
+
+const newWords = words;
+
+newWords[1] = "kiwi";
+
+console.log(words);
+console.log(newWords);*/
