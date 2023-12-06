ca1c897f6a8bdce59c4a508bb4c675a48b96dfc0
dhanshree <dhanshreewagh16598@gmail.com>
2023-12-06 15:08:18 +0530

    loops

---
+++ Learning-Front-End-Development/3.learnjs/6.Loops/script.js
@@ -0,0 +1,26 @@
+ // for (let index = 1; index < 0; index = index +2) {
+  //  console.log("Dhara", index);
+// }
+
+// let step  = 0;
+// while (step < 5) { 
+   // console.log("step", step);
+   // step += 1;
+// }
+
+// let step = 0;
+// do {
+//   console.log("step", step);
+  //   step += 1;
+
+// } while (step < 5);
+
+let step = 0;
+while (step <5) {
+    step += 1;
+    if (step === 2) {
+        continue;
+    }
+    console.log("step",step);
+
+}
--- Learning-Front-End-Development/3.learnjs/script.js
+++
@@ -1,3 +0,0 @@
-for (let index = 1; index < 0; index = index +2) {
-    console.log("Dhara", index);
-}
