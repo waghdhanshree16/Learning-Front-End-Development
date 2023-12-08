d6fa790762c7a62452a5121af4d892478ec6645f
dhanshree <dhanshreewagh16598@gmail.com>
2023-12-08 14:59:39 +0530

    small changes

--- Learning-Front-End-Development/3.learnjs/8.Function basics/nestedFunctions.js
+++ Learning-Front-End-Development/3.learnjs/8.Function basics/nestedFunctions.js
@@ -7,5 +7,17 @@ function addSquares(a, b) {
     }
     return sa + sb;
 }
-
 console.log(addSquares(3, 4));
+
+
+// let a = 2
+// let b = 3
+
+// sqr1 = a * a
+// sqr2 = b * b
+
+// // console.log(sqr1)
+// // console.log(sqr2)
+
+// addSquares = sqr1 + sqr2
+// console.log(addSquares)
