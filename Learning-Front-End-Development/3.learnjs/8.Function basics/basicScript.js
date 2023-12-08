5bea1e84ca14c1f9149bd4affed26e1b6c4fbb43
dhanshree <dhanshreewagh16598@gmail.com>
2023-12-08 13:58:20 +0530

    function

---
+++ Learning-Front-End-Development/3.learnjs/8.Function basics/basicScript.js
@@ -0,0 +1,30 @@
+function sumOfTwoNumbers(a, b) {
+    const sum = a + b;
+    console.log("result is", sum);
+ } 
+ 
+ const a = 124;
+ const b = 4;
+ sumOfTwoNumbers(a, b) ;
+ //
+ // do something else
+ //
+ const c = 12;
+ const d = 9;  
+ sumOfTwoNumbers(c, d); 
+//
+// do something else 
+//
+const e = 2;
+ const f = 4;
+ sumOfTwoNumbers(e, f); 
+
+ const square = function (num) {
+    return num * num;
+ };
+
+ console.log(square);
+
+ console.log(square(4));
+
+ 
